cc58eb62fbf5a8c806edf74cc694bb6b8b98b89a
Magdalena Pal <magdalena.pal90@gmail.com>
2018-01-04 22:44:54 +0100

    Add test

---
+++ test/hello.js
@@ -0,0 +1,31 @@
+const Hello = artifacts.require('Hello')
+
+contract('Hello', function(accounts) {
+  it('sets the first account as the contract creator', async function() {
+    // This give a truffle abstraction which allow us to interact with our contracts.
+    const contract = await Hello.deployed()
+
+    // Once we have the "contract" we can make calls or transations and then assert.
+    // The following is making a call to get the creator.
+    const creator = await contract.getCreator()
+
+    assert.equal(creator, accounts[0], 'main account is the creator')
+  })
+  it('has hello as initial message ', async function() {
+    const contract = await Hello.deployed()
+    const message = await contract.getMessage()
+
+    assert.equal(message, 'hello', 'message is hello')
+  })
+  it('changes the message via setMessage', async function() {
+    const contract = await Hello.deployed()
+
+    // Execute a transaction and change the state of the contract.
+    await contract.setMessage('hola mundo')
+
+    // Get the new state.
+    const message = await contract.getMessage()
+
+    assert.equal(message, 'hola mundo', 'message is hola mundo')
+  })
+})
